9907cdfc09128650f9d983e09aedae8e1687b321
“Josefia <josefia.magagnoli@gmail.com>
2019-05-20 19:01:43 -0400

    Teacher Table

---
+++ database/migrations/20190520184727_teacher_table.js
@@ -0,0 +1,26 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema
+    .createTable('teachers', table => {
+        table
+            .increments()
+        table
+            .string('name', 250)
+            .notNullable()
+        table
+            .string('notes', 1200)
+        table
+            .integer('admin_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('admin')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE')
+    })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.scema 
+    .dropTableIfExists('teachers')
+};
